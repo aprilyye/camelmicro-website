e418d67b750edbd1fcf5556ce57c4639be2c1b15
April Ye <aprileaf@dhcp-clara-5064.redrover.cornell.edu>
2018-04-29 10:47:29 -0400

    m3 product pg

--- product.php
+++ product.php
@@ -14,6 +14,33 @@ $current_page = 'product.php';
 </head>
 
 <body>
+  <div id="content-wrap">
+    <article id="content">
+      <h2 id="product-title">K32W0x: Dual-core, large memory, high security, Bluetooth 5 + 802.15.4 MCU</h2>
+      <!-- <img id="imgopen" alt="Image Upload" src="uploads/product1.png" width="400" height="550"/> -->
 
+
+      <!-- Use css and javascript to style into accordion  -->
+      <h2 id="product-title">Features</h2>
+      <button class="accordion">Info 1</button>
+      <div class="panel">
+        <p>Lorem ipsum...</p>
+      </div>
+
+      <button class="accordion">Info 2</button>
+      <div class="panel">
+        <p>Lorem ipsum...</p>
+      </div>
+
+      <button class="accordion">Info 3</button>
+      <div class="panel">
+        <p>Lorem ipsum...</p>
+      </div>
+
+      <!-- Overiew description -->
+      <h2 id="product-title">Overview</h2>
+      <p> text </p>
+    </article>
+  </div>
 </body>
 </html>
