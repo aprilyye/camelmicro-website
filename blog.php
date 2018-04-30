e31a3fe318990782fc89a74e173ed885f418aa72
Prajanan Senthilkumar <ps7336@cornell.edu>
2018-04-30 18:21:20 -0400

    Blog input feature for blog updates

--- blog.php
+++ blog.php
@@ -2,6 +2,7 @@
 include('includes/init.php');
 include('includes/header.php');
 $current_page = 'blog.php';
+$db=open_or_init_sqlite_db('blog.sqlite', "init/init.sql");
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,6 +15,33 @@ $current_page = 'blog.php';
 </head>
 
 <body>
+  <?php
+  echo '<br>';
+  echo
+  "<form action='' method='post'>
+  <input type='text' placeholder ='Title' name='Title'>
+  <br>
+  <textarea ='text' placeholder ='Text' rows='4' cols='50' name='Text'></textarea>
+  <input type='submit' name='Post' value='Add Tag' />
+  </form>";
+  if(isset($_POST['Post'])){
+    $Title = $_POST['Title'];
+    $Text = $_POST['Text'];
+    $sql2 = "INSERT INTO blog (Title, Text) VALUES (:Title, :Text)"; $params2 = array(
+  ':Title' => $Title,
+  ':Text' => $Text,
+);
+  $addition2 = exec_sql_query($db, $sql2, $params2);
 
+  }
+
+$records = exec_sql_query($db, "SELECT * FROM blog")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($records as $record){
+  echo "<h2>" . $record["Title"] . "</h2>";
+  echo "<p>" .$record["Text"]. "</p>";
+  echo "<br>";
+}
+ ?>
 </body>
 </html>
--- init/init.sql
+++ init/init.sql
@@ -18,3 +18,14 @@ INSERT INTO applications (name) VALUES ('Medical');
 INSERT INTO applications (name) VALUES ('Portable and Wireless');
 INSERT INTO applications (name) VALUES ('Motor Control');
 INSERT INTO applications (name) VALUES ('Networking and Telecommunications');
+
+
+CREATE TABLE blog (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `Title` TEXT,
+	`Date`	TEXT,
+	`Text` TEXT
+
+);
+
+INSERT INTO blog (Title, Date, Text) VALUES ('Sample Blog Post', 'April 2018', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
