ba1c6762ac441b50c3d4952f39aff52bd34ffed2
Aaliya Khan <aaliyavajih@dhcp-mcfad-11411.eduroam.cornell.edu>
2018-04-29 23:58:57 -0400

    applications.php and diagram.php and SQL applications table

--- init/init.sql
+++ init/init.sql
@@ -1,3 +1,20 @@
 /* TODO: create tables */
 
 /* TODO: initial seed data */
+CREATE TABLE `applications` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT,
+	`image`	TEXT
+);
+
+INSERT INTO applications (name) VALUES ('Automotive');
+INSERT INTO applications (name) VALUES ('Industrial');
+INSERT INTO applications (name) VALUES ('Military and Aerospace');
+INSERT INTO applications (name) VALUES ('Power Supply');
+INSERT INTO applications (name) VALUES ('Computing and Peripherals');
+INSERT INTO applications (name) VALUES ('LED Lighting');
+INSERT INTO applications (name) VALUES ('Consumer');
+INSERT INTO applications (name) VALUES ('Medical');
+INSERT INTO applications (name) VALUES ('Portable and Wireless');
+INSERT INTO applications (name) VALUES ('Motor Control');
+INSERT INTO applications (name) VALUES ('Networking and Telecommunications');
