fc637000ff99f8ba00f2b46be97271f0e5193891
Iris Zhao <iz29@cornell.edu>
2018-05-06 14:23:01 -0400

    Changes

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $current_page = 'contact.php';
 </head>
 
 <body>
-<div>
-<h1> Camel Micro </h1>
+<div id='home'>
+<h5> Camel Micro </h5>
   <p>Camel microelectronics is a fabless integrated circuit design company with focus on 32bit  MCU(micro-controller unit) products for medical, industrial automation, instrument, automotive, electronic and IoT (Internet of Things) devices.  Camel microelectronics is headquartered in Shanghai, China. Camel microelectronics also has R&D center in silicon valley, USA.  Camel microelectronics provides, world class performance, quality and cost, 32bit MCU solutions to medical appliance, advanced motor control, industrial automation, and IoT devices.
 
 Our experienced engineering and logistic teams provide complete support to our customers during every stage of their product life cycle, from choosing the right MCU to optimize system performance and cost during development phase, to providing JIT (Just In Time) delivery during production phase. Over the years, Camel microelectronics has proven to be a Solidly Dependable memory solution partner to our customers.
