8dde70c27568243aaf99b7738f1807c7aed1ec7a
April Ye <aprileaf@dhcp-gs-6525.redrover.cornell.edu>
2018-05-12 15:22:05 -0400

    product + standardized css

--- product.php
+++ product.php
@@ -17,7 +17,7 @@ $current_page = 'product.php';
   <div id="content-wrap">
     <article id="content">
       <h2 id="product-title">M2: 32-bit Mixed-Signal Processor</h2>
-      <producth1> M2 is a CMOS Mixed Signal Processor (MSP) for intelligent
+      <producth1>M2 is a CMOS Mixed Signal Processor (MSP) for intelligent
         medical/sensor applications with 32-bit embedded computing power.
         It provides a complete set of system modules to support embedded data
         acquisition, algorithm analysis, internal controlling, external interrupt,
