ba27fabbeabdf09c2bd9224cda3efa6d4bbd561c
Prajanan Senthilkumar <ps7336@cornell.edu>
2018-05-15 18:43:58 -0400

    first initialization of login

--- blog.php
+++ blog.php
@@ -3,7 +3,7 @@ include('includes/init.php');
 $current_page_id = 'blog';
 include('includes/header.php');
 
-$db=open_or_init_sqlite_db('blog.sqlite', "init/init.sql");
+$db=open_or_init_sqlite_db('login.sqlite', "init/init.sql");
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,35 +11,45 @@ $db=open_or_init_sqlite_db('blog.sqlite', "init/init.sql");
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel='stylesheet' href='styles/main.css'/>
   <title>Blog</title>
 </head>
 
 <body>
-  <div>
-  <h1> Blogs</h1>
-  </div>
   <?php
-  echo '<div id=\'blog\'>';
-  echo '<h2>' ;
-  echo 'Add a Post' ;
-  echo '</h2>';
-  echo
-  "<div id='entry'><form action='' method='post'>
-  <input type='text' placeholder ='Title' name='Title'>
-  <textarea ='text' placeholder ='Text' name='Text'></textarea>
-  <input type='submit' name='Post' value='Add Tag' />
-  </form></div>";
-  if(isset($_POST['Post'])){
-    $Title = $_POST['Title'];
-    $Text = $_POST['Text'];
-    $sql2 = "INSERT INTO blog (Title, Text) VALUES (:Title, :Text)"; $params2 = array(
-  ':Title' => $Title,
-  ':Text' => $Text,
-);
-  $addition2 = exec_sql_query($db, $sql2, $params2);
+  //initialize admin data
+  $access = exec_sql_query($db, "SELECT * FROM Accounts")->fetchAll(PDO::FETCH_ASSOC);
+?>
 
+<div>
+  <h1>Blogs</h1>
+</div>
+<?php
+$verify1 = login();
+var_dump($verify1);
+  if ($verify1 != NULL){
+    echo '<div id=\'blog\'>';
+    echo '<h2>' ;
+    echo 'Moderator: Add a Post' ;
+    echo '</h2>';
+    echo
+    "<div id='entry'><form action='' method='post'>
+    <input type='text' placeholder ='Title' name='Title'>
+    <textarea ='text' placeholder ='Text' name='Text'></textarea>
+    <input type='submit' name='Post' value='Add Tag' />
+    </form></div>";
+    if(isset($_POST['Post'])){
+      $Title = $_POST['Title'];
+      $Text = $_POST['Text'];
+      $sql2 = "INSERT INTO blog (Title, Text) VALUES (:Title, :Text)"; $params2 = array(
+        ':Title' => $Title,
+        ':Text' => $Text,
+      );
+      $addition2 = exec_sql_query($db, $sql2, $params2);
+
+    }
+  }else{
   }
+
 
 $records = exec_sql_query($db, "SELECT * FROM blog")->fetchAll(PDO::FETCH_ASSOC);
 
