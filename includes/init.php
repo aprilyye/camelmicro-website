c8111253ec18e3a25c8edde17a17b9832021e3b2
Prajanan Senthilkumar <ps7336@cornell.edu>
2018-05-16 13:48:45 -0400

    class change

--- includes/init.php
+++ includes/init.php
@@ -21,7 +21,7 @@ function push_message($message) {
 function print_out() {
   global $messages;
   foreach ($messages as $message) {
-    echo '<p id= "centerlinemessage">' . htmlspecialchars($message) . "</p>\n";
+    echo '<p class= "centerlinemessage">' . htmlspecialchars($message) . "</p>\n";
   }
 }
 
